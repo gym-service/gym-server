83d90ea193a6126d0b7dff468cc5ebe95897c904
Mauro Bianchi <bianchimro@gmail.com>
2015-11-20 16:39:23 +0100

    admin for gyusers

--- gym_server/tenant_admin/admin.py
+++ gym_server/tenant_admin/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from gym_users.models import GymUserInfo
+
 admin_site = admin.AdminSite(name='tenant')
 admin_site.register(get_user_model())
+admin_site.register(GymUserInfo)
 
