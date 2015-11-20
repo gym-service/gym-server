fd26f68e06ba53f4219ed192c02ed899f0a18ea0
Mauro Bianchi <bianchimro@gmail.com>
2015-11-20 16:00:56 +0100

    signals post

--- gym_server/customers/signals.py
+++ gym_server/customers/signals.py
@@ -5,8 +5,8 @@ from tenant_schemas.utils import tenant_context
 from customers.models import Client
 from .utils import create_superuser, create_or_update_superuser
 
-@receiver(pre_save, sender=Client, dispatch_uid="pre_save_client_create_superuser")
-def model_pre_save(sender, **kwargs):
+@receiver(post_save, sender=Client, dispatch_uid="post_save_client_create_superuser")
+def model_post_save(sender, **kwargs):
     instance = kwargs['instance']
     #we manage the public instance by ourselves..
     public_schema_name = getattr(settings, 'PUBLIC_SCHEMA_NAME', 'public')
@@ -15,9 +15,10 @@ def model_pre_save(sender, **kwargs):
 
     if not instance.pk:
         create_superuser(instance, instance.email)
+    """
     else:
         old_tenant_pk = instance.pk
         with tenant_context(instance):
             old_tenant = Client.objects.get(pk=old_tenant_pk)
             create_or_update_superuser(instance, old_tenant.email, instance.email)
-        
+    """
