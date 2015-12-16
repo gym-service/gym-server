92e7b0ea5220a724e19f2c5ac2c8d7b832d2c6b2
Mauro Bianchi <bianchimro@gmail.com>
2015-12-16 13:49:02 +0100

    evento

--- gym_server/gym_contents/models.py
+++ gym_server/gym_contents/models.py
@@ -19,6 +19,16 @@ class BaseContent(models.Model):
     updated = models.DateTimeField(auto_now=True)
     stato = models.IntegerField(choices = STATO_CHOICES, default=0, blank=True)
     promosso = models.BooleanField(blank=True, default=False)
+    url = models.URLField(blank=True, null=True)
 
     class Meta:
         abstract = True
+
+
+class Evento(BaseContent):
+
+    immagine = models.ImageField(null=True, blank=True, upload_to="immagini")
+    allegato = models.FileField(null=True, blank=True, upload_to="allegati")
+    data = models.DataField(null=True, blank=True)
+    luogo = models.CharField(max_length=30, null=True, blank=True)
+    descrizione_luogo = models.TextField(null=True, blank=True)
