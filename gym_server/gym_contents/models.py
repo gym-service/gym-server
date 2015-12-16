8f5633defd9bbf66cc7558fec8e50526ff0be3a4
Mauro Bianchi <bianchimro@gmail.com>
2015-12-16 13:45:21 +0100

    added promosso, sottotitolo

--- gym_server/gym_contents/models.py
+++ gym_server/gym_contents/models.py
@@ -12,11 +12,13 @@ class BaseContent(models.Model):
 
     ordine = models.IntegerField(null=False, blank=True, default=0)
     titolo = models.CharField(max_length=80, null=False)
+    sottotitolo = models.CharField(max_length=80, null=True, blank=True)
     descrizione = models.CharField(max_length=2000, null=True, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     stato = models.IntegerField(choices = STATO_CHOICES, default=0, blank=True)
+    promosso = models.BooleanField(blank=True, default=False)
 
     class Meta:
         abstract = True
